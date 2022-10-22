1e7f6ea59f88aa0001b3b192f8b98f69d19cbe2f
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-22 07:59:43 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
 class Dog
+    # attr_accessor:name
+    # attr_accessor:breed
+    #setter
+    def name=(name)
+        @name = name
+        
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getter
+    def name
+        @name 
+    end
+    def breed
+        @breed 
+    end
+
 
 end
+
+dog1 = Dog.new
+
+dog1.name = "simba"
+dog1.breed = "hybrid"
+
+puts (dog1.name)
+puts (dog1.breed)
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+person1 = Person.new
+person1.name = "Mercy"
+person1.job = "developer"
+
+puts (person1.name)
+puts (person1.job)
